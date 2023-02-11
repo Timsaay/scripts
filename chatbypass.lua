b146b3441d031b86d33ebced9903ae39db3aebe6
Timsaay <79137069+Timsaay@users.noreply.github.com>
2023-02-12 10:05:14 +1100

    Delete chatbypass.lua

--- chatbypass.lua
+++
@@ -1,7 +0,0 @@
-local Main = game:GetService("CoreGui").Rayfield:WaitForChild("Main") 
-while Main.visible == true and Main.Topbar.Title.Text ~= 'PapaHub' do
-    
-wait()
-game:GetService("CoreGui").Rayfield.Main.Topbar.Title.Text = 'PapaHub'
-print('Loop')
-end 
