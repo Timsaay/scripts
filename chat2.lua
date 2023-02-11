0cf3931af5e9e911bbc2a0ec3a2c7ba97210f020
Timsaay <79137069+Timsaay@users.noreply.github.com>
2023-02-12 09:44:02 +1100

    Create chat2.lua

---
+++ chat2.lua
@@ -0,0 +1,12 @@
+while true do
+wait()
+if game:GetService("CoreGui").Rayfield:WaitForChild("Main").Visible then
+    game:GetService("CoreGui").Rayfield.Main.Topbar.Title.Text = 'PapaHub'
+    print("Main UI has loaded and been changed to PapaHub")
+end
+
+if game:GetService("CoreGui").Rayfield.Main.Topbar.Title.Text == 'PapaHub'
+
+break
+print("breaking while true")
+end 
