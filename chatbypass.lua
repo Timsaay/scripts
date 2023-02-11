8be8952bd20a08e496ed675a5191cbd6543e63df
Timsaay <79137069+Timsaay@users.noreply.github.com>
2023-02-12 10:04:48 +1100

    Update chatbypass.lua

--- chatbypass.lua
+++ chatbypass.lua
@@ -1,13 +1,7 @@
-while true do
+local Main = game:GetService("CoreGui").Rayfield:WaitForChild("Main") 
+while Main.visible == true and Main.Topbar.Title.Text ~= 'PapaHub' do
+    
 wait()
-if game:GetService("CoreGui").Rayfield:WaitForChild("Main").Visible then
-    game:GetService("CoreGui").Rayfield.Main.Topbar.Title.Text = 'PapaHub'
-    print("Main UI has loaded and been changed to PapaHub")
-end
-
-if game:GetService("CoreGui").Rayfield.Main.Topbar.Title.Text == 'PapaHub' then 
-break
-print("breaking while true")
-end
-
+game:GetService("CoreGui").Rayfield.Main.Topbar.Title.Text = 'PapaHub'
+print('Loop')
 end 
