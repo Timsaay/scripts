cefd5d58a9be128683c44cc3c5dff735da88b8cc
Timsaay <79137069+Timsaay@users.noreply.github.com>
2023-02-25 10:10:53 +1100

    Update legendsofspeed.lua

--- legendsofspeed.lua
+++ legendsofspeed.lua
@@ -1,2 +1,2 @@
-local script = 'https://raw.githubusercontent.com/ReeAndArceus/Legends-Of-Speed-Gui/main/Gui'
+script = 'https://raw.githubusercontent.com/ReeAndArceus/Legends-Of-Speed-Gui/main/Gui'
 loadstring(game:HttpGet(('https://raw.githubusercontent.com/Timsaay/scripts/main/KEYSYSTEM.lua'),true))()
